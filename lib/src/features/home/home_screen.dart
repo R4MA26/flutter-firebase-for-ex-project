7651132e241a44cf29aa8e9e27004a4c00017055
R4MA26 <ramaismaya25@gmail.com>
2023-09-06 16:18:31 +0800

    login with google sign in

--- lib/src/features/home/home_screen.dart
+++ lib/src/features/home/home_screen.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:learn_firebase/src/common_widgets/card_button_widget.dart';
 import 'package:learn_firebase/src/features/auth/presentation/login_controller.dart';
+import 'package:learn_firebase/src/features/home/home_controller.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final controller = ref.read(loginControllerProvider.notifier);
+    final controllerLogin = ref.read(loginControllerProvider.notifier);
+    final controller = ref.read(homeControllerProvider.notifier);
     final user = controller.getCurrentUser;
     return Scaffold(
       body: Center(
@@ -24,7 +26,7 @@ class HomeScreen extends ConsumerWidget {
             CardButtonWidget(
               title: 'Logout',
               onTap: () {
-                controller.signOut(context);
+                controllerLogin.signOut(context);
               },
             ),
           ],
